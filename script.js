e55998b545a980f92ba7de0e42fe7897bd6cc3b2
Sunjida-Srijoni <sunjidasrijoni@gmail.com>
2024-02-28 21:19:32 +0600

    set the slideshow for infinite time

--- script.js
+++ script.js
@@ -2,10 +2,31 @@ const slideShowElements = document.querySelectorAll(".single-element");
 
 
 console.log(slideShowElements);
+let countElements = 1;
 
 setInterval(() => {
+
+    countElements++;
+
+    if(countElements == 5)
+    {
+        countElements = 2;
+
+    }
+ 
     let currentElement = document.querySelector(".current");
     currentElement.classList.remove("current");
-    currentElement.nextElementSibling.classList.add("current");
+
+    if(countElements > slideShowElements.length)
+    {
+        slideShowElements[0].classList.add("current");
+    }
+    else{
+        currentElement.nextElementSibling.classList.add("current");
+    }
+    
 
 }, 2000)
+
+
+// if(countElements > slideShowElements.length+1 || countElements <= slideShowElements.length)
